daaa4dbcaf0a5ae13aaa02bbc979395746d7b2e5
John McFarland <John.McFarland.690@gmail.com>
2016-06-22 15:35:08 -0400

    Added documentation to demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -10,23 +10,32 @@
 using namespace std;
 
 int main() {
+	//Declares the infix and postfix variables. 
+	//Prompts the user for input
+	//Lets the user input an infix expression
+	//Outputs the infix expession
 	string infix, postfixExp = " ";
 	cout << "Enter an infix expression: "<<endl;
 	getline(cin, infix);
 	cout << "Infix is: "<< infix << endl;
 
+
+	//Tries to convert the infix expression to a postfix expression
+	//If the Expression can't be converted, it throws an exception
 	try{
 		postfixExp = infixToPostfix(infix);
 	}catch (EvalException e) {
 		cerr << e.getMessage() <<endl;
 	}
 
+	//Prints the postfix expression
 	cout << "Postfix is " << postfixExp << endl;
 
+	//Tries to evaluate the new postfix expression
+	//If the expression fails to evaluate, it throws an exception.
 	try {
 		cout << "Evaluation is: " << evalPostfix(postfixExp) << endl;
 	} catch (EvalException e) {
 			cerr << e.getMessage() << endl;
 	}
-	return 0;
 }
